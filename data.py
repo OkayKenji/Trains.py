757dde8068e5436fa57d3bed9e240d2d2b269985
Kenji <70079754+OkayKenji@users.noreply.github.com>
2022-08-01 17:17:04 -0400

    Update train display order
    Cleaned it up so mnrr trains match the mnrr timetables

--- data.py
+++ data.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import os as o
+from datetime import timedelta as td
+
 
 def getServices(calendar_dates,railroad):
     calendar_dates = calendar_dates.astype({'date':'str'})
@@ -23,12 +26,12 @@ def getDate():
 
 def loadData():
     while True:
-        railroad = input("Enter the railroad (MNRR/LIRR): ")
+        railroad = input("Enter the railroad (MNRR/LIRR): ").lower()
         
-        if railroad.lower() == 'mnrr':
+        if railroad == 'mnrr':
             print("Wait a moment as we read in data...")
             return pd.read_csv(f'data/MNRR/calendar_dates.csv'), pd.read_csv(f'data/MNRR/routes.csv'), pd.read_csv(f'data/MNRR/stop_times.csv'), pd.read_csv(f'data/MNRR/stops.csv'),  pd.read_csv(f'data/MNRR/trips.csv'), pd.read_csv(f'data/MNRR/calendar_dates.csv'), pd.read_csv(f'data/MNRR/transfers.csv'), railroad
-        elif railroad.lower() == 'lirr':
+        elif railroad == 'lirr':
             print("Wait a moment as we read in data...")
             return pd.read_csv(f'data/LIRR/calendar_dates.csv'), pd.read_csv(f'data/LIRR/routes.csv'), pd.read_csv(f'data/LIRR/stop_times.csv'), pd.read_csv(f'data/LIRR/stops.csv'),  pd.read_csv(f'data/LIRR/trips.csv'), None, None, railroad
         else:
@@ -83,8 +86,97 @@ def getStopingTrainsAtStop(railroad,stops,reformated):
         else:
             break
    
+    allTrains = []
     count = 0
-    for i in reformated:
+    for train in reformated:
+        stops = train[0].to_numpy()
+        for stop in stops:
+            if stop[3] == int(num): # stop[3] contains the id of the station
+                if railroad == 'mnrr':
+                    allTrains.append([stop[1], stop[7], stop[8], train[1], train[2], train[3],stops[0][1]])
+                    # print(f'{ii[1]} on track {ii[7]} {ii[8]} Train: {i[1]} to: {i[2]} going: {i[3]}')
+                else:
+                    allTrains.append([stop[1], None, None, train[1], train[2], train[3],stops[0][1]])
+                    # print(f'{ii[1]} Train: {i[1]} to: {i[2]} going: {i[3]}')
+                count += 1
+
+    
+    allTrains = sorted(allTrains, key=lambda allTrains: allTrains[0]) # sort by time it get's to the station
+
+    if railroad == 'mnrr':
+        allTrains = cleanAllTrains(allTrains)
+
+    for i in allTrains:
+        if railroad == 'mnrr':
+            if i[2] == 'H':
+                print(f'At {i[0]} on track {i[1]} Train: {i[3]} to: {i[4]} going: {i[5]} *Train may depart 5 minutes earlier than the time shown')
+            else:
+                print(f'At {i[0]} on track {i[1]} Train: {i[3]} to: {i[4]} going: {i[5]}')
+        else:
+            print(f'At {i[0]} Train: {i[3]} to: {i[4]} going: {i[5]}')
+    # print(ah)
+
+
+def cleanAllTrains(allTrains):
+    for i,ele in enumerate(allTrains):
+        if int(ele[0][0:2])>23:
+            allTrains[i][0] = f'0{str(int(ele[0][0:2])%24)}{ele[0][2:8]}'
+    count = 0
+    for i,ele in enumerate(allTrains):
+        if int(ele[6][0:2])<4:
+            count+=1
+        else:
+            break
+    for AXN in range(count):
+        moveBack = allTrains.pop(0)
+        allTrains.append(moveBack)
+
+    return allTrains
+
+
+
+
+def cvtStringToNumber(stop_name,stops):
+    trainName = stops[stops.stop_name == stop_name]
+    if trainName.empty == True:
+        return None
+    else:
+        return trainName.stop_id.to_numpy()[0]
+
+def aaa(railroad,stops,reformated):
+    listOfStations = []
+
+    while True:
+        stop_name = input("Enter first station name: ")
+        num = cvtStringToNumber(stop_name,stops)
+        if num == None:
+            print("Error, station not found!")
+        else:
+            listOfStations.append(num)
+            break
+
+    while True:
+        stop_name = input("Enter intermidiate station name: ")
+        if stop_name == 'exit':
+            break
+        num = cvtStringToNumber(stop_name,stops)
+        if num == None:
+            print("Error, station not found!")
+        else:
+            listOfStations.append(num)
+    while True:
+        stop_name = input("Enter last station name: ")
+        num = cvtStringToNumber(stop_name,stops)
+        if num == None:
+            print("Error, station not found!")
+        else:
+            listOfStations.append(num)
+            break
+
+    
+    count = 0
+    reformated_cpy = reformated
+    for i in reformated_cpy:
         arr = i[0].to_numpy()
         for ii in arr:
             if ii[3] == int(num):
@@ -93,14 +185,6 @@ def getStopingTrainsAtStop(railroad,stops,reformated):
                 else:
                     print(f'{ii[1]} Train: {i[1]} to: {i[2]} going: {i[3]}')
                 count += 1
-    print(count)
-
-def cvtStringToNumber(stop_name,stops):
-    trainName = stops[stops.stop_name == stop_name]
-    if trainName.empty == True:
-        return None
-    else:
-        return trainName.stop_id.to_numpy()[0]
 
 def reformat(listOfTrains,stop_times): 
     reformated = []
@@ -127,9 +211,9 @@ def main():
 
     userRequest = ''
     while True:
-        userRequest = input('What to do (A/B/C/D/E/Exit)\nA: Overall stats. on trains\nB: Search by train number\nC: Search Train Station\nD: Dump into file\nE: Dump to JSON\nInput: ')
+        userRequest = input('What to do (A/B/C/D/E/Exit)\nA: Overall stats. on trains\nB: Search by train number\nC: Search Train Station (only trains that stop there)\nD: Dump into file\nE: Dump to JSON\nInput: ')
         if (userRequest == '-1' or userRequest == 'Exit'):
-            break
+            exit(0)
         elif userRequest == 'A':
             trainAnaysis(listOfTrains,routes)
         elif userRequest == 'B':
@@ -140,6 +224,13 @@ def main():
             myFile = open("output.txt","w")
             myFile.write(listOfTrains.to_string())
             myFile.close()
+            print('Done')
+        elif userRequest == 'E':
+            print('opps not done yet.')
+        elif userRequest == 'F':
+            aaa(railroad,stops,reformated)
+        else:
+            print('Try again.')
 
 if __name__ == "__main__":
     main()
