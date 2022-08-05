6715e3673732a316b4ac240ab8732ce8a9c2bdcf
Kenji <70079754+OkayKenji@users.noreply.github.com>
2022-08-04 21:59:00 -0400

    getInfoByMultiStops
    find what train to take, mnrr trains after midnight are 24+ time

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import os as o
+import os
+import time as t
 from datetime import timedelta as td
 
 
@@ -76,51 +77,79 @@ def listStops(trip_stops,stops):
         stop_name = stops[stops.stop_id == stop[3]].stop_name.to_numpy()[0]
         print(stop_name,stop[1])
 
-def getStopingTrainsAtStop(railroad,stops,reformated):
+def getStopingTrainsAtStop(routes,railroad,stops,reformated,asNumber=None):
 
-    while True:
-        stop_name = input("Enter station name: ")
-        num = cvtStringToNumber(stop_name,stops)
-        if num == None:
-            print("Error, station not found!")
-        else:
-            break
+    if asNumber == None:
+        while True:
+            stop_name = input("Enter station name: ")
+            num = cvtStringToNumber(stop_name,stops)
+            if num == None:
+                print("Error, station not found!")
+            else:
+                break
+    else:
+        num = asNumber
    
     allTrains = []
     count = 0
     for train in reformated:
-        stops = train[0].to_numpy()
-        for stop in stops:
+        stopsArr = train[0].to_numpy()
+        for stop in stopsArr:
             if stop[3] == int(num): # stop[3] contains the id of the station
                 if railroad == 'mnrr':
-                    allTrains.append([stop[1], stop[7], stop[8], train[1], train[2], train[3],stops[0][1]])
-                    # print(f'{ii[1]} on track {ii[7]} {ii[8]} Train: {i[1]} to: {i[2]} going: {i[3]}')
+                    allTrains.append([stop[1], stop[7], stop[8], train[1], train[2], train[3],stopsArr[0][1],train[4],cvtNumberToString(stopsArr[0][3],stops),stopsArr])
+                    # print(f'{ii[1]} on track {ii[7]} {ii[8]} Train: {i[1]} to: {i[2]} going: {i[3]}') 
                 else:
-                    allTrains.append([stop[1], None, None, train[1], train[2], train[3],stops[0][1]])
+                    allTrains.append([stop[1], None, None, train[1], train[2], train[3],stopsArr[0][1],train[4],cvtNumberToString(stopsArr[0][3],stops),stopsArr])
                     # print(f'{ii[1]} Train: {i[1]} to: {i[2]} going: {i[3]}')
                 count += 1
 
-    
     allTrains = sorted(allTrains, key=lambda allTrains: allTrains[0]) # sort by time it get's to the station
 
     if railroad == 'mnrr':
-        allTrains = cleanAllTrains(allTrains)
+        allTrains = cleanAllTrains(allTrains,routes)
+    else:
+        for i,ele in enumerate(allTrains):
+            allTrains[i][7] = cvtRouteStringToNumber(routes,allTrains[i][7])
+            if ele[5] == '1':
+                allTrains[i][5] = 'West'
+            else:
+                allTrains[i][5] = 'East'
+
 
     for i in allTrains:
         if railroad == 'mnrr':
             if i[2] == 'H':
-                print(f'At {i[0]} on track {i[1]} Train: {i[3]} to: {i[4]} going: {i[5]} *Train may depart 5 minutes earlier than the time shown')
+                print(f'{i[0]} {i[7]} Line train to {i[4]}. Arrivng track {i[1]} going {i[5]}. (Train: {i[3]}) *Train may depart 5 minutes earlier than the time shown')
             else:
-                print(f'At {i[0]} on track {i[1]} Train: {i[3]} to: {i[4]} going: {i[5]}')
+                print(f'{i[0]} {i[7]} Line train to {i[4]}. Arrivng track {i[1]} going {i[5]}. (Train: {i[3]})')
         else:
-            print(f'At {i[0]} Train: {i[3]} to: {i[4]} going: {i[5]}')
+            print(f'{i[0]} {i[7]} train to {i[4]}. Going: {i[5]}. (Train: {i[3]})')
     # print(ah)
 
+    
+    aaabbb = pd.DataFrame(data=allTrains)
+    myFile = open("output.txt","w")
+    myFile.write(aaabbb.to_string())
+    myFile.close()
 
-def cleanAllTrains(allTrains):
+    return allTrains
+
+
+def cleanAllTrains(allTrains,routes):
     for i,ele in enumerate(allTrains):
-        if int(ele[0][0:2])>23:
-            allTrains[i][0] = f'0{str(int(ele[0][0:2])%24)}{ele[0][2:8]}'
+        allTrains[i][7] = cvtRouteStringToNumber(routes,allTrains[i][7]) 
+
+        if ele[5] == '1':
+            allTrains[i][5] = 'South'
+        else:
+            allTrains[i][5] = 'North'
+
+        # if int(ele[0][0:2])>23:
+        #    allTrains[i][0] = f'0{str(int(ele[0][0:2])%24)}{ele[0][2:8]}'
+
+
+
     count = 0
     for i,ele in enumerate(allTrains):
         if int(ele[6][0:2])<4:
@@ -129,12 +158,12 @@ def cleanAllTrains(allTrains):
             break
     for AXN in range(count):
         moveBack = allTrains.pop(0)
+        moveBack[0] = f'{str(int(moveBack[0][0:2])+24)}{moveBack[0][2:8]}'
         allTrains.append(moveBack)
 
+    allTrains = sorted(allTrains, key=lambda allTrains: allTrains[0])
+
     return allTrains
-
-
-
 
 def cvtStringToNumber(stop_name,stops):
     trainName = stops[stops.stop_name == stop_name]
@@ -143,7 +172,21 @@ def cvtStringToNumber(stop_name,stops):
     else:
         return trainName.stop_id.to_numpy()[0]
 
-def aaa(railroad,stops,reformated):
+def cvtNumberToString(stop_num,stops):
+    stopName = stops[stops.stop_id == stop_num]
+    if stopName.empty == True:
+        return None
+    else:
+        return stopName.stop_name.to_numpy()[0]
+
+def cvtRouteStringToNumber(routes,route_id):
+    route_name = routes[routes.route_id == route_id]
+    if route_name.empty == True:
+        return None
+    else:
+        return route_name.route_long_name.to_numpy()[0]
+
+def getInfoByMultiStops(railroad,stops,reformated,routes):
     listOfStations = []
 
     while True:
@@ -173,25 +216,51 @@ def aaa(railroad,stops,reformated):
             listOfStations.append(num)
             break
 
+    print(listOfStations)
+    allTrains = getStopingTrainsAtStop(routes,railroad,stops,reformated,asNumber=listOfStations[0])
     
-    count = 0
-    reformated_cpy = reformated
-    for i in reformated_cpy:
-        arr = i[0].to_numpy()
-        for ii in arr:
-            if ii[3] == int(num):
-                if railroad == 'mnrr':
-                    print(f'{ii[1]} on track {ii[7]} {ii[8]} Train: {i[1]} to: {i[2]} going: {i[3]}')
+    os.system('cls')
+
+    indexToRemove = len(allTrains)-1
+    doNotRemove = False
+    for index in range(1,len(listOfStations)):
+        selectStations = listOfStations[index]
+        indexToRemove = len(allTrains)-1
+        # print(selectStations)
+        for train in reversed(allTrains):
+            
+            please_Remove = False
+            for stop in train[9]:
+                if stop[3] == int(selectStations):
+                    please_Remove = False
+                    break
                 else:
-                    print(f'{ii[1]} Train: {i[1]} to: {i[2]} going: {i[3]}')
-                count += 1
+                    please_Remove = True
+
+
+            if please_Remove:
+                allTrains.pop(indexToRemove)
+
+            indexToRemove -= 1
+    allTrains = sorted(allTrains, key=lambda allTrains: allTrains[5])
+
+    for train in allTrains:
+        print(f'Train: {train[3]}, departing {train[0]}, making {str(len(train[9]))} stops. Goes from {train[8]} to {train[4]} going {train[5]}.')
+    return allTrains
 
 def reformat(listOfTrains,stop_times): 
     reformated = []
+    tempPercent = 0
+    print(f'{0}%...')
     for index,train in enumerate(listOfTrains.trip_id):
-        temp = [stop_times[stop_times.trip_id == train],listOfTrains.trip_short_name.iloc[index],listOfTrains.trip_headsign.iloc[index],listOfTrains.direction_id.iloc[index],listOfTrains.shape_id.iloc[index]]
+        if ((index/len(listOfTrains))*100 > tempPercent+10 ):
+            print(f'{tempPercent+10}%...')
+            tempPercent += 10
+        
+        # print(f'Percent complete: {str((index/len(listOfTrains))*100)}%')
+        temp = [stop_times[stop_times.trip_id == train],listOfTrains.trip_short_name.iloc[index],listOfTrains.trip_headsign.iloc[index],listOfTrains.direction_id.iloc[index],listOfTrains.route_id.iloc[index],listOfTrains.shape_id.iloc[index]]
         reformated.append(temp)
-
+    print(f'{100}%...')
     return reformated
 
 
@@ -211,7 +280,8 @@ def main():
 
     userRequest = ''
     while True:
-        userRequest = input('What to do (A/B/C/D/E/Exit)\nA: Overall stats. on trains\nB: Search by train number\nC: Search Train Station (only trains that stop there)\nD: Dump into file\nE: Dump to JSON\nInput: ')
+        os.system('cls')
+        userRequest = input('What to do (A/B/C/D/E/F/Exit)\nA: Overall stats. on trains\nB: Search by train number\nC: Search Train Station (only trains that stop there)\nD: Dump into file\nE: Dump to JSON\nF: Multiple Stations\nInput: ')
         if (userRequest == '-1' or userRequest == 'Exit'):
             exit(0)
         elif userRequest == 'A':
@@ -219,7 +289,7 @@ def main():
         elif userRequest == 'B':
             getInfoByTrainNumber(listOfTrains,stop_times,routes,stops)
         elif userRequest == 'C':
-            getStopingTrainsAtStop(railroad,stops,reformated)
+            getStopingTrainsAtStop(routes,railroad,stops,reformated)
         elif userRequest == 'D':
             myFile = open("output.txt","w")
             myFile.write(listOfTrains.to_string())
@@ -228,9 +298,13 @@ def main():
         elif userRequest == 'E':
             print('opps not done yet.')
         elif userRequest == 'F':
-            aaa(railroad,stops,reformated)
+            getInfoByMultiStops(railroad,stops,reformated,routes)
         else:
             print('Try again.')
+            t.sleep(0.5)
+            continue
+
+        input()
 
 if __name__ == "__main__":
     main()
